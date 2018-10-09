ef137d2f376e00984a9eaf7bf4160b92bd062a93
Infant Frontender <newbornfrontender@gmail.com>
2018-10-09 15:16:45 +0300

    Добавил список с ссылками

---
+++ src/components/VList.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+    <li
+      v-for="item of list"
+      :key="item.id"
+    >
+      <v-link :href="item.path">{{ item.text }}</v-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+  import VLink from './VLink.vue';
+
+  export default {
+    name: 'VList',
+
+    props: {
+      list: {
+        type: Array,
+        required: true,
+      },
+    },
+
+    components: {
+      VLink,
+    },
+  };
+</script>

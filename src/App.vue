5cea3268e6167ee16fac3694fe5fc5bc25303c1a
Infant Frontender <newbornfrontender@gmail.com>
2018-10-09 14:46:43 +0300

    Переписал компонент

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,33 @@
+<template>
+  <section>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <v-link href="/">Home</v-link>
+          </li>
+
+          <li>
+            <v-link href="/about">About</v-link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+    <main>
+      <slot></slot>
+    </main>
+
+    <footer>Footer</footer>
+  </section>
+</template>
+
 <script>
+  import VLink from './components/VLink.vue';
+
   export default {
-    render (h) { return h(
-      'main', [
-        this.$slots.default,
-      ],
-    )},
+    components: {
+      VLink,
+    },
   };
 </script>

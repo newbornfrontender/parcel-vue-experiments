5d1ddf03ce09fd450e5ec24a7fef02ad589f513b
Infant Frontender <newbornfrontender@gmail.com>
2018-10-09 15:17:05 +0300

    Добавил кастомную навигацию

---
+++ src/components/VNav.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav>
+    <v-list :list="navList" />
+  </nav>
+</template>
+
+<script>
+  import VList from './VList.vue';
+
+  export default {
+    name: 'VNav',
+
+    components: {
+      VList,
+    },
+
+    data: () => ({
+      navList: [{
+        text: 'Home',
+        path: '/',
+      }, {
+        text: 'About',
+        path: '/about',
+      }],
+    }),
+  };
+</script>

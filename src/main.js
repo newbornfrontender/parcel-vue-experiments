b3da9f4b6e2a0737097a09d78a8ceac1896b8c9d
Infant Frontender <newbornfrontender@gmail.com>
2018-10-09 13:12:25 +0300

    Настроил роутинг

--- src/main.js
+++ src/main.js
@@ -1,10 +1,31 @@
 import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
+import App from './App';
+import Home from './views/Home.vue';
+import About from './views/About.vue';
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  render: h => h(App)
+const routes = {
+  '/': Home,
+  '/about': About,
+};
+
+const vm = new Vue({
+  data: {
+    currentRoute: window.location.pathname,
+  },
+  computed: {
+    ViewComponent () {
+      return routes[this.currentRoute];
+    },
+  },
+  render (h) { return h(
+    App, [ h(
+      this.ViewComponent,
+    )],
+  )},
 }).$mount('#app');
+
+window.addEventListener('popstate', () => {
+  vm.currentRoute = window.location.pathname;
+});
